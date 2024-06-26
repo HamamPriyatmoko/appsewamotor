e0125ee98ae61d396c6bbfbc0128f106ea27e0ba
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-06-26 20:59:20 +0700

    menambahkan profile bar

--- lib/widgets/HomeAppBar.dart
+++ lib/widgets/HomeAppBar.dart
@@ -7,13 +7,21 @@ class HomeAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      padding: EdgeInsets.all(25),
+      padding: EdgeInsets.all(15),
       child: Row(
         children: [
-          Icon(
-            Icons.sort,
-            size: 30,
-            color: Colors.blue,
+          InkWell(
+            onTap: () {
+              // Aksi yang ingin dilakukan ketika ikon ditekan
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Menu icon tapped')),
+              );
+            },
+            child: Icon(
+              Icons.sort,
+              size: 30,
+              color: Colors.blue,
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(left: 10),
@@ -21,7 +29,33 @@ class HomeAppBar extends StatelessWidget {
               "Remo",
               style: TextStyle(fontSize: 20),
             ),
-          )
+          ),
+          Spacer(),
+          InkWell(
+            onTap: () {
+              // Aksi yang ingin dilakukan ketika profil ditekan
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Profile tapped')),
+              );
+              // Atau navigasi ke halaman profil
+              // Navigator.push(context, MaterialPageRoute(builder: (context) => ProfilePage()));
+            },
+            child: Row(
+              children: [
+                Text(
+                  "Hi, Hamam  ",
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/vario.jpg'),
+                  radius: 20,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
